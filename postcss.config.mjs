25a7891827b266a1f61203a28facf3a5a9feb1e1
Moozzeess <136218873+Moozzeess@users.noreply.github.com>
2025-07-31 19:46:03 -0600

    postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    // postcss-preset-env: https://cssdb.org/
-    // Adds vendor prefixes and other PostCSS features based on your browserslist config.
-    // tailwindcss: https://tailwindcss.com/docs/using-with-postcss
-    // PostCSS plugin to process your CSS with Tailwind.
     tailwindcss: {},
   },
 };
